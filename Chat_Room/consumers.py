363b5b9471f14b97e4e6e2965f5c7c03d38c84ef
vjtrollkilla <vjsingh284@gmail.com>
2021-10-03 22:11:43 +0400

    Updating Chat_room/consumers.py and chatroom.html
    This update was to allow the rooms to have user logged in users to send messages with their name in it.

--- Chat_Room/consumers.py
+++ Chat_Room/consumers.py
@@ -1,7 +1,17 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
-
+# await self.channel_layer.group_send(
+       #     self.room_group_name,
+        #    {
+       #         'type':'tester_message',
+       #         'tester':'Hello world',
+        #    }
+        #)
+    #async def tester_message(self,event):
+      #  tester = event['tester']
+      #  await self.send(text_data=json.dumps({
+      #      'tester':tester,
+      #  }))
 import json
-
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
@@ -13,22 +23,36 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
         await self.accept()
-        
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type':'tester_message',
-                'tester':'Hello world',
-            }
-        )
-    async def tester_message(self,event):
-        tester = event['tester']
-        await self.send(text_data=json.dumps({
-            'tester':tester,
-        }))
-    async def disconnect(self, code):
+
+    async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = text_data_json['username']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chatroom_message',
+                'message': message,
+                'username': username,
+            }
+        )
+
+    async def chatroom_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+        }))
+
+    pass
