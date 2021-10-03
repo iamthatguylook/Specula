3bb06c8c292696344610ee73858fb9833d40d7c1
vjtrollkilla <vjsingh284@gmail.com>
2021-10-03 14:25:14 +0400

    Adding chat room urls to settings.py

--- Specula/urls.py
+++ Specula/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('',include('User.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('chat/',include('chat.urls'))
     
 ]  
