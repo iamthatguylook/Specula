7dee19d5777be6e506f9277e0bf4fe3ce85cbc3a
vjtrollkilla <vjsingh284@gmail.com>
2021-10-21 13:42:28 +0400

    Remove login from installed apps as Registeration and User exist

--- Specula/settings.py
+++ Specula/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Registeration',
-    'Login',
+   
     'rest_framework',
     'User',
     'rest_framework.authtoken',
